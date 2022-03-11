7c07dc943148cbc3c84bf1d14fa31b7f6ebe5848
Muhani99 <allenmuhani@gmail.com>
2022-03-11 15:32:21 +0300

    added first aautomates test

---
+++ test/WakalaEscrow.ts
@@ -0,0 +1,21 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Test Initialize transactions.", function () {
+  it("Test initialize deposit transactions.", async function () {
+    const [owner] = await ethers.getSigners();
+
+    const WakalaEscrow = await ethers.getContractFactory("WakalaEscrow");
+    const wakalaEscrow = await WakalaEscrow.deploy();
+    await wakalaEscrow.deployed();
+
+    expect(await wakalaEscrow.getNextTransactionIndex()).to.equal(0);
+
+    expect(await wakalaEscrow.initializeDepositTransaction(100000000))
+      .to.emit("WakalaEscrow", "TransactionInitEvent")
+          .withArgs(0, owner.getAddress());
+
+
+    expect(await wakalaEscrow.getNextTransactionIndex()).to.equal(1);
+  });
+});
