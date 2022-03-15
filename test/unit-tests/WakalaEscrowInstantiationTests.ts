eab34d5b3213294d4fe084569ae524694860cd46
Allen <allenmuhani@gmail.com>
2022-03-15 08:47:19 +0300

    added test utils

--- test/unit-tests/WakalaEscrowInstantiationTests.ts
+++ test/unit-tests/WakalaEscrowInstantiationTests.ts
@@ -1,12 +1,20 @@
 import { expect } from "chai";
 import { TestUtil } from "../testutils";
 
-describe("Test instantiation values.", function () {
+describe("Test withdrawal transaction end to end.", function () {
   it("Test", async function () {
     const testUtil = new TestUtil();
     await testUtil.intit();
 
-    expect(await testUtil.wakalaEscrow.countSuccessfulTransactions()).to.equal(0);
+    await testUtil.cUSD.approve(testUtil.wakalaEscrow.address, 10);
+
     expect(await testUtil.wakalaEscrow.getNextTransactionIndex()).to.equal(0);
+
+    expect(await testUtil.wakalaEscrow.initializeWithdrawalTransaction(5))
+      .to.emit("WakalaEscrow", "TransactionInitEvent")
+      .withArgs(0, testUtil.user1Address.getAddress());
+
+    expect(await testUtil.wakalaEscrow.getNextTransactionIndex()).to.equal(1);
   });
 });
+
